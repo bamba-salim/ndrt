cd2144e79475c51e0c3af1647ec701963867e868
bamba-salim <01salim.bamba@gmail.com>
2020-12-11 07:40:25 +0100

    user: login -> update system

--- config/config.php
+++ config/config.php
@@ -5,17 +5,15 @@ date_default_timezone_set("Europe/Paris");
 
 require "constant.php";
 
-
 // BDD
 require "./class/_general.class.php";
 require "./class/__db.class.php";
 $DB = new DB();
 function DB()
 {
-  $DB = new DB();
-  return $DB;
+    $DB = new DB();
+    return $DB;
 }
-
 
 require "./class/adresse.class.php";
 require "./class/category.class.php";
@@ -27,7 +25,6 @@ require "./class/user.class.php";
 require "./class/_admin.class.php";
 require "./class/_cart.class.php";
 require "./class/_form.class.php";
-require "./class/_log.class.php";
 require "./class/_navigation.class.php";
 require "./class/_modal.class.php";
 require "./class/_site.class.php";
@@ -37,7 +34,6 @@ $ADRESSE = new ADRESSE();
 $CART = new CART($DB);
 $CATEGORY = new CATEGORY();
 $FORM = new FORM();
-$LOGIN = new LOG();
 $MAIL = new MAIL();
 $ORDER = new ORDER();
 $PRODUCT = new PRODUCT();
@@ -46,6 +42,5 @@ $USER = new USER();
 $NAV = new NAV();
 
 $MODAL = new MODAL();
-
 
 require "module_html.php";
